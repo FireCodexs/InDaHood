80d21fc86ff6cbfe5c32ebc8726a852301cc1468
Sophie Senge <sophieperks@gmail.com>
2023-03-15 19:02:42 +0000

    added modal and shopping car icon

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Cart from "./components/cart/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const App = () => {
-  return <div>App</div>;
+function App(){
+  return (<div>
+    <Router>
+      <Routes>
+        <Route path="/cart"/>
+      </Routes>
+    </Router>
+    
+<Cart/>
+  </div>
+  
+  )
 };
 
 export default App;
