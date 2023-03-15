429a5c6f6ed7c1f236f9682363c92c73f25ddf49
Mihai-Andrei Grigoras <andrei.eu.mihaii@gmail.com>
2023-03-15 20:37:25 +0000

    added card layout

--- src/app.js
+++ src/app.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import Card from "./components/card/Card";
+import products from "./datasource.json";
+import Wrapper from "./components/wrapper/Wrapper";
 
-const App = () => {
-  return <div>App</div>;
-};
+class App extends Component {
+  // Setting this.state.products to the products json array
+  state = {
+    products,
+  };
+
+  // Map over this.state.friends and render a Card component for each object
+  render() {
+    return (
+      <Wrapper>
+        {this.state.products.map((product) => (
+          <Card
+            key={product.id}
+            name={product.name}
+            image={product.image}
+            hoverImage={product.hoverImage}
+            sizes={product.sizes}
+            category={product.category}
+            tags={product.tags}
+            price={product.price}
+            location={product.location}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
+}
 
 export default App;
---
+++ src/components/wrapper/Wrapper.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Wrapper = ({ children }) => {
+  return (
+    <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
+            {children}
+    </div>
+  );
+};
+
+export default Wrapper;
