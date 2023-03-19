be86a410bfb92d2b161faaa6fa98ae504449f888
Milena <mitessari@yahoo.com.br>
2023-03-19 19:04:54 +0000

    added navbar and started contact us section

--- src/app.js
+++ src/app.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from "./components/navbar/Navbar.jsx";
+import Men from "./components/pages/Men.jsx";
+import Contact from "./components/pages/Contact.jsx";
+import SignUp from "./components/pages/SignUp";
+import Women from "./components/pages/Women.jsx"
+//
 
 const App = () => {
-  return <div>App</div>;
+  return( 
+<>
+      <Router>
+        
+          <Navbar />
+          {/* Wrap Route elements in a Routes component */}
+          <Routes  >
+            {/* Define routes using the Route component to render different page components at different paths */}
+            {/* Define a default route that will render the Home component */}
+            <Route path="/women" element={<Women />} />
+            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/men" element={<Men />} />
+            {/* <Route path="/" element={<About />} /> */}
+            <Route path="/contact" element={<Contact />} />
+            {/* Define a route that will have descendant routes */}
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        
+      </Router>
+       
+        </>
+  );
 };
 
 export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+//import { NavLink } from 'react-router-dom';
+import {IoMdContact} from 'react-icons/io';
+
+function Navbar() {
+  return( 
+  <nav className="nav">
+    <a href="/" className="site-title">InDaHood</a>
+    <ul>
+        <li>
+            <a href="/women">Women</a>
+        </li>
+        <li>
+            <a href="/men">Men</a>
+        </li>
+        <li>
+            <a href="/contact"><IoMdContact/></a>
+        </li>
+        
+        <button type="button" className="btn btn-outline-light">Sign In</button>
+        <button className="btn btn-primary" href="#">Sign Up</button>
+        
+
+        
+    </ul>
+  </nav> 
+  )
+  
+  
+}
+
+export default Navbar;
