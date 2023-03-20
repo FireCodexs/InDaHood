771125d00702cdc6d9db6bb9f0581d8600ed76ad
Sophie Senge <sophieperks@gmail.com>
2023-03-20 18:37:30 +0100

    hero section is responsive

--- src/components/Hero/HeroBanner.jsx
+++ src/components/Hero/HeroBanner.jsx
@@ -3,18 +3,20 @@ import Cart from '../cart/Cart';
 
 function HeroBanner() {
   return (
-    <div className="w-full h-3/5 bg-[url('https://images.unsplash.com/photo-1529333166437-7750a6dd5a70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80')] bg-cover bg-center flex justify-center items-center">
-    <h1 className="text-2xl md:text-4xl mb-3">Kimia-UI Tutorial</h1>
-    <p className="mb-3">
-      Kimia-ui is a collection of React components built with tailwindcss and
-      fully customizable that allows you to be more productive
+    <div className="w-full h-4/5 bg-[url('https://images.unsplash.com/photo-1529333166437-7750a6dd5a70?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80')] bg-cover bg-center">
+      <div className="bg-gradient-to-b from-yellow-300 to-transparent py-3">
+
+    
+    <h1 className="subpixel-antialiased font-cursive text-center pt-20 text-9xl text-[#FFA600]">Spring</h1>
+    <h1 className="subpixel-antialiased font-semibold text-center pt-2 pb-2 text-8xl mb-3 text-orange-600">SALE</h1>
+    <div className='lg:bg-white lg:mx-96 p-0'>
+    <p className=" text-center font-semibold text-rose-600 mb-3 text-4xl">
+     10% off everything
     </p>
-    <p>This is some text.</p>
+    </div>
+   
+    </div>
     <Cart/>
-    <p>This is another text.</p>
-    <button className="text-sm px-6 py-1 mt-4 text-white active:bg-yellow-500 focus:outline-none border-solid border">
-      Learn more
-    </button>
   </div>
   )
 }
