6bec0d7c5efb47091f4976e05734f8af03b9692a
Sophie Senge <sophieperks@gmail.com>
2023-03-21 19:06:38 +0100

    added offset correction for smooth scroll

--- src/components/Hero/HeroBanner.jsx
+++ src/components/Hero/HeroBanner.jsx
@@ -14,7 +14,7 @@ function HeroBanner() {
         <h1 className=" subpixel-antialiased font-cursive text-center pt-20 text-9xl text-[#FFA600]">Spring</h1>
         <h1 className="subpixel-antialiased font-semibold text-center pt-2 pb-2 text-8xl mb-3 text-orange-600">SALE</h1>
         <div className="justify-center flex ">
-        <Link to="cards" spy={true} smooth={true} offset={50} duration={500} className="relative px-6 py-3 font-bold text-white rounded-lg group">
+        <Link to="cards" spy={true} smooth={true} offset={-40} duration={500} className="relative px-6 py-3 font-bold text-white rounded-lg group cursor-pointer">
 <span className="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-orange-500 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
 <span className="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-yellow-600 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
 <span className="relative">Shop Sale!</span>
