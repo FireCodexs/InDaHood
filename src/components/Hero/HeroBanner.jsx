548f7c0c96fa0de2bba08f00aafbd2884af2d76f
Sophie Senge <sophieperks@gmail.com>
2023-03-21 07:18:05 +0100

    centered button

--- src/components/Hero/HeroBanner.jsx
+++ src/components/Hero/HeroBanner.jsx
@@ -7,13 +7,16 @@ function HeroBanner() {
       <div className="bg-gradient-to-b from-yellow-300 to-transparent py-3">
 
 
-        <h1 className="subpixel-antialiased font-cursive text-center pt-20 text-9xl text-[#FFA600]">Spring</h1>
+        <h1 className=" subpixel-antialiased font-cursive text-center pt-20 text-9xl text-[#FFA600]">Spring</h1>
         <h1 className="subpixel-antialiased font-semibold text-center pt-2 pb-2 text-8xl mb-3 text-orange-600">SALE</h1>
-        <a href="#_" className="m-auto left-0 right-0 relative px-6 py-3 font-bold text-white rounded-lg group">
+        <div className="justify-center flex ">
+        <a href="#_" className="relative px-6 py-3 font-bold text-white rounded-lg group">
 <span className="absolute inset-0 w-full h-full transition duration-300 transform -translate-x-1 -translate-y-1 bg-orange-500 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0"></span>
 <span className="absolute inset-0 w-full h-full transition duration-300 transform translate-x-1 translate-y-1 bg-yellow-600 ease opacity-80 group-hover:translate-x-0 group-hover:translate-y-0 mix-blend-screen"></span>
 <span className="relative">Shop Sale!</span>
 </a>
+        </div>
+        
 
       </div>
       <Cart />
